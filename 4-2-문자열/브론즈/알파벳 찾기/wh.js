ec5f1d48520a5347d7fe2847df990509ecb2d64e
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-09 10:19:57 +0900

    refactor: input.txt 위치 변경

---
+++ 4-2-문자열/브론즈/알파벳 찾기/wh.js
@@ -0,0 +1,8 @@
+// 문제출처: https://www.acmicpc.net/problem/10809
+const fs = require("fs");
+const file =
+  process.platform === "linux" ? "dev/stdin" : "../../../input-wh.txt";
+const splitter = file ? "\n" : "\r\n";
+const input = fs.readFileSync(file).toString().trim().split(splitter);
+
+console.log(input);
