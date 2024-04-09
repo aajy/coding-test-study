0afc30dea43469cad66cfc60cc5c4db5db42efe0
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-10 08:48:32 +0900

    문제풀이: 알파벳 찾기
    - a-z 배열 만들기 방법 획득
    - indexOf 사용법 획득 (배열에서 인덱스 찾기)

--- 4-2-문자열/브론즈/알파벳 찾기/wh.js
+++ 4-2-문자열/브론즈/알파벳 찾기/wh.js
@@ -5,9 +5,21 @@ const file =
 const splitter = file ? "\n" : "\r\n";
 const input = fs.readFileSync(file).toString().trim().split(splitter);
 
-console.log("asd JEJEje".split(" ")[1].toLowerCase());
+const targetInput = input[0].split("");
+const arr = "qwertyuiopasdfghjklzxcvbnm";
+const alphabets = arr.split("").sort();
+
+for (let i = 0; i < alphabets.length; i++) {
+  console.log(targetInput.indexOf(alphabets[i]));
+}
+/*
+
+*/
 
 /*
   영문자의 댓문자 변경은
   toLowerCase, toUpperCase 를 활용한다.
 */
+
+/* [a - z] 배열은 암기를 해야한다. */
+// 객체.속성 꼴은 대상의 행위, 대상의 값 으로 이해한다. (주어가 아니라는 말)
