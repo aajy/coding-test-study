bd70bcc264be12a455a93595571a21e9288fcacb
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-14 22:57:28 +0900

    알고리즘: 1차원배열을 순회하면서 등장횟수를 기록한 table 만들기
    - if(!table[el]) 통해 분기처리하며 속성을 추가하는게 중요하다.

---
+++ 문자열-4-2/실버/팰린드롬 만들기/wh.js
@@ -0,0 +1,17 @@
+// 문제출처: https://www.acmicpc.net/problem/1213
+const fs = require("fs");
+const file =
+  process.platform === "linux" ? "dev/stdin" : "../../../input-wh.txt";
+const splitter = file ? "\n" : "\r\n";
+const input = fs.readFileSync(file).toString().trim().split(splitter);
+
+// 1차원배열을 순회하면서 table을 만들기
+const inputArr = input[0].split("");
+const table = {};
+
+inputArr.forEach((el) => {
+  if (!table[el]) table[el] = 1;
+  else table[el]++;
+});
+
+console.log(table);
