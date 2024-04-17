484bde8c85ae7309e756aa27c6cd8c6fe438849b
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-17 14:28:43 +0900

    알고리즘: new Map 을 사용해 객체를 만드는 과정
    - table.has(el) ? table.set(el, table.get(el) + 1) : table.set(el, 1);
    - [...table] table 을 구조분해 한 후 배열에 재 할당하는 과정
    - sort((a, b) => a[0].localCompare(b[0])) 은 사전식으로 정렬하는 과정

--- 문자열-4-2/실버/팰린드롬 만들기/wh.js
+++ 문자열-4-2/실버/팰린드롬 만들기/wh.js
@@ -1,15 +1,21 @@
 // 문제출처: https://www.acmicpc.net/problem/1213
+// 문제풀이: https://breathtaking-life.tistory.com/166#toc-[%EC%BD%94%EB%93%9C&%ED%92%80%EC%9D%B4]
 const fs = require("fs");
 const file =
   process.platform === "linux" ? "dev/stdin" : "../../../input-wh.txt";
 const splitter = file ? "\n" : "\r\n";
 const input = fs.readFileSync(file).toString().trim().split(splitter);
-
-// 1차원배열을 순회하면서 table을 만들기 === 객체만들기
 const inputArr = input[0].split("");
-const table = new Map();
 
-inputArr.forEach((el) => {});
+let table = new Map();
+// Map 객체를 활용할 경우 for of 를 사용할 수 있다.
+for (const el of inputArr) {
+  table.has(el) ? table.set(el, table.get(el) + 1) : table.set(el, 1);
+}
+
+// [...table] 은 table 객체의 모든 요소를 추출한 후 배열에 할당하는 과정을 축약한 것
+// (a, b) => a[0].localCompare(b[0])); 는 사전식 정렬을 의미함
+table = Object.values([...table].sort((a, b) => a[0].localeCompare(b[0])));
 
 // 개수를 통해 규칙찾기
 /*
@@ -23,12 +29,5 @@ inputArr.forEach((el) => {});
 /* 
   table 만들기: Map vs Object
 
-  Object은 iteration이 귀찮음
-  Map은 익숙해지면 함수형으로 테이블을 다룰 수 있고, iteration이 가능해짐
-
-  오버로딩과 오버라이딩의 차이점을 아나요?
-
-  오버로딩은 이름은 같지만 시그니쳐가 다른 함수를 만들어서 사용하는 것
-  오버라이딩은 상속관계에서 자식클래스에서 부모클래스의 메소드와 시그니처가 같은 메소드를 만드는 것
 
 */
